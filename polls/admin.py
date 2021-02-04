b659f378ba7920b2588956d2d7021738d795c2d7
9bini <gotn0727@gmail.com>
2021-02-04 14:28:00 +0900

    끝

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,35 @@
+# 기본적으로 페이징 기능 제공
 from django.contrib import admin
 
+# 관련된 객체를 추가하려면 해당 객체를 import 해야됨!
 from .models import Question, Choice
 
-admin.site.register(Question)
+# StackedInline => TabularInline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # 기본적으로 3가지 선택 항목을 제공
+    # 초이스 객체가 n개 있으면 추가로 3가지 선택 항목제공
+    extra = 3
+
+# admin 클래서 생성 -> admin.site.register() 두번째 인수로 전달
+class QuestionAdmin(admin.ModelAdmin):
+    # 발행일(pub_date), 설문(question_text) 순으로 필드 앞에 오게 만듭니다.
+    # fileds = ['pub_date', 'question_text']
+    fieldsets = [
+        # fieldsets 첫 번째 인자(None, 'Date information')는 제목을 의미한다.
+        (None,               {'fields': ['question_text']}),
+        # classes???
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    # 기본적으로 __str__을 표시합니다.
+    # 개별 필드를 표시 할 수 있는 경우 가끔 도움이 될 수 있습니다.
+    # list_displays는 객체의 변경 목록 페이지에서 열로 표시 할 파두 아름들의 튜플입니다.
+    # was_published_recently 임의이 메소드의 출력에 의한 정렬은 지원되지 않는다.
+    # 그외에는 정렬을 지원한다.
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date'] # 데이터 필터링
+    search_fields = ['question_text'] # 필드 검색(like)
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -12,8 +12,12 @@ class Question(models.Model):
     
     def was_published_recently(self):
         now = timezone.now()
-        return (now - datetime.timedelta(days=1)) <= self.pub_date <= now
-    
+        return (now - datetime.timedelta(days=1)) <= self.pub_date <= now        
+
+    # 메소드에 몇시가 속성을 부가하면 관리자 변경 목록이 보기 좋게 향상됩니다.
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
